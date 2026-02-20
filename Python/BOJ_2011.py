376138ca4b435156c848027f642289a2af0ba47f
Gangto326 <kangsansam@naver.com>
2026-02-20 14:39:16 +0900

    BOJ 2011 암호코드.py

--- Python/BOJ_2011.py
+++ Python/BOJ_2011.py
@@ -2,36 +2,29 @@ import sys
 
 def solve():
     MOD = 1_000_000
-    password = list(map(int, list(sys.stdin.readline().strip())))
-    DP = [[0] * 27 for _ in range(len(password))]
-    DP[0][password[0]] = 1
+    password = list(sys.stdin.readline().strip())
+    N = len(password)
 
-    if not password[0]:
+    if N == 0 or password[0] == "0":
         print(0)
         return
-    
-    if len(password) >= 2:
-        sum_num = password[0] * 10 + password[1]
-        DP[1][password[1]] = 1 if password[1] else 0
 
-        if 0 < sum_num <= 26:
-            DP[1][sum_num] = 1
+    DP = [0] * (N+1)
+    DP[0] = 1
+    DP[1] = 1
 
-
-    for i in range(2, len(password)):
-        if password[i-1]:
-            sum_num = password[i-1] * 10 + password[i]
-
-            if 0 < sum_num <= 26:
-                DP[i][sum_num] += sum(DP[i-2]) % MOD
-
-        if not password[i]:
-            continue
+    for i in range(2, N+1):
+        if int(password[i-1]):
+            DP[i] += DP[i-1]
         
-        DP[i][password[i]] += sum(DP[i-1]) % MOD
-
-    print(sum(DP[-1]) % MOD)
-
+        disit = int(password[i-2]) * 10 + int(password[i-1])
+        if 10 <= disit <= 26:
+            DP[i] += DP[i-2]
+        
+        DP[-1] %= MOD
+    
+    print(DP[-1])
+    
 
 if __name__ == "__main__":
     solve()
