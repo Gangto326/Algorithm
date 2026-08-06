86b9be35ea00dc75526c0f3dba4478af35526347
Gangto326 <kangsansam@naver.com>
2026-08-06 18:58:29 +0900

    Jungol 1809 탑.py

---
+++ Python/Jungol_1809.py
@@ -0,0 +1,26 @@
+import sys
+
+def solve():
+    read = sys.stdin.readline
+    N = int(read())
+    num_list = [float('inf')] + list(map(int, read().split()))
+
+    stack = [0]
+    answer_list = [-1] * (N + 1)
+    
+    for i in range(1, N + 1):
+        while num_list[stack[-1]] < num_list[i]:
+            num = stack.pop()
+            answer_list[num] = stack[-1]
+        
+        stack.append(i)
+
+    while len(stack) != 1:
+        num = stack.pop()
+        answer_list[num] = stack[-1]
+
+    print(" ".join(map(str, answer_list[1:])))
+    
+
+if __name__ == "__main__":
+    solve()
